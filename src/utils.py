314ea2bfe30bcfb3ec4613bdbb80c6d0c1f1e667
Lei Wang <wangleiphy@gmail.com>
2024-01-22 15:10:16 +0800

    load breaks on this file

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import jax
 import jax.numpy as jnp
+import numpy as np
 import pandas as pd
 from pymatgen.core import Structure
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
@@ -27,15 +28,16 @@ def GLXAW_from_structures(structures, atom_types, wyck_types, n_max, dim):
     X = [] # fractional coordinate 
     AW = [] # atom type and wyck type; 0 for placeholder
     for i, structure in enumerate(structures):
+        print (structure)
         analyzer = SpacegroupAnalyzer(structure, symprec=0.1)   # a looser tolerance of 0.1 (the value used in Materials Project) is often needed.
         refined_structure = analyzer.get_refined_structure()
         analyzer = SpacegroupAnalyzer(refined_structure)
         symmetrized_structure = analyzer.get_symmetrized_structure()
         
-        assert (structure.num_sites == symmetrized_structure.num_sites)
         
         g = analyzer.get_space_group_number()
         G.append (g)
+        print (i, g, structure.num_sites, symmetrized_structure.num_sites)
         abc = tuple([l/symmetrized_structure.num_sites**(1./3.) for l in symmetrized_structure.lattice.abc])
         L.append (abc + symmetrized_structure.lattice.angles) # scale length with number of total atoms
         num_sites = len(symmetrized_structure.equivalent_sites)
@@ -57,18 +59,21 @@ def GLXAW_from_structures(structures, atom_types, wyck_types, n_max, dim):
             fc.append( x)
             print ('g, a, w, m, symbol', g, a, w, m, wyckoff_symbol, x)
         #sort atoms according to lexicographic order of wyckoff symbol
-        ws, aw, fc = zip(*sorted(zip(ws, aw, fc)))
-        ws = list(ws)
-        aw = list(aw)
-        fc = list(fc)
+        idx = np.argsort(ws)
+        ws = np.array(ws)[idx]
+        aw = jnp.array(aw)[idx]
+        fc = jnp.array(fc)[idx].reshape(num_sites, dim)
         print (ws) 
+        
+        aw = jnp.concatenate([aw, 
+                              jnp.full((n_max - num_sites, ), 0)], 
+                              axis = 0)
+        fc = jnp.concatenate([fc, 
+                             jnp.full((n_max - num_sites, dim), 1e10)], 
+                             axis = 0)
 
-        AW.append( aw + [0] * (n_max - num_sites) )
-        frac_coords = jnp.array(fc).reshape(num_sites, dim)
-        frac_coords = jnp.concatenate([frac_coords, 
-                                       jnp.full((n_max - num_sites, dim), 1e10)], 
-                                       axis = 0)
-        X.append (frac_coords)  
+        AW.append( aw )
+        X.append ( fc )  
  
         print ('===================================')
 
@@ -88,13 +93,14 @@ def GLXAW_from_file(csv_file, atom_types, wyck_types, n_max, dim):
 
 if __name__=='__main__':
     atom_types = 119
-    wyck_types = 10
-    n_max = 5
+    wyck_types = 30
+    n_max = 20
     dim = 3
 
     #csv_file = '/home/wanglei/cdvae/data/perov_5/val.csv'
-    csv_file = '../data/mini.csv'
+    #csv_file = '../data/mini.csv'
     #csv_file = '/home/wanglei/cdvae/data/mp_20/train.csv'
+    csv_file = './mp.csv'
     G, L, X, AW = GLXAW_from_file(csv_file, atom_types, wyck_types, n_max, dim)
     
     print (G.shape)
