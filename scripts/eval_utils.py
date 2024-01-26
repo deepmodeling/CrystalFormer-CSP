a98a07579fa233d4df3f288ddd4aea6737795316
Zhendong Cao <zdcao@iphy.ac.cn>
2024-01-26 22:01:29 +0800

    remove eval_utils.py and add config.py

--- scripts/eval_utils.py
+++ scripts/eval_utils.py
@@ -4,13 +4,14 @@ import numpy as np
 import smact
 from smact.screening import pauling_test
 
-from data_utils import chemical_symbols
+from config import *
+from elements import element_list
 
 
 def smact_validity(comp, count,
                    use_pauling_test=True,
                    include_alloys=True):
-    elem_symbols = tuple([chemical_symbols[elem] for elem in comp])
+    elem_symbols = tuple([element_list[elem] for elem in comp])
     space = smact.element_dictionary(elem_symbols)
     smact_elems = [e[1] for e in space.items()]
     electronegs = [e.pauling_eneg for e in smact_elems]
