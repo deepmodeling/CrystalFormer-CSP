2fb8dc2eea7205a77cc28acf68de711560d7b3d2
Lei Wang <wangleiphy@gmail.com>
2024-02-12 09:23:28 +0800

    reveal the rational mismatch in test_sampling

--- tests/test_sampling.py
+++ tests/test_sampling.py
@@ -10,6 +10,20 @@ def test_symops():
         m_max = mult_table[g-1, w_max].item()
         x = project_x(g, w_max, m_max, w[None, ...], x[None, ...], idx[None, ...])
         return x[0]
+    
+    # these two tests shows that depending on the z coordinate (which is supposed to be rationals)
+    # the WP can be recoginized differently, resulting different x
+    # this motivate that we either predict idx in [1, m], or we predict all fc once there is a continuous dof
+    g = 167 
+    w = jnp.array(5)
+    idx = jnp.array(5)
+    x = jnp.array([0.123, 0.123, 0.75])
+    y = map_fn(g, w, x, idx)
+    assert jnp.allclose(y, jnp.array([0.123, 0.123, 0.75]))
+
+    x = jnp.array([0.123, 0.123, 0.25])
+    y = map_fn(g, w, x, idx)
+    assert jnp.allclose(y, jnp.array([0.877, 0.877, 0.75]))
 
     g = 225
     w = jnp.array(5)
