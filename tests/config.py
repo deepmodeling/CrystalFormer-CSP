a45a1415e328c494be3f64bccce1478acbd88fbd
Lei Wang <wangleiphy@gmail.com>
2024-03-14 14:25:15 +0800

    rm unused tests

--- tests/config.py
+++ tests/config.py
@@ -3,7 +3,6 @@ testdir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(testdir, "../src"))
 
 import jax
-jax.config.update("jax_enable_x64", True)
 import jax.numpy as jnp
 import numpy as np 
 import haiku as hk
