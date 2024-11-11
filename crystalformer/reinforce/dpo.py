6209b2c581de253a18b3666573aa5faf65a2bc3b
Zhendong Cao <zdcao@iphy.ac.cn>
2024-11-11 23:10:48 +0800

    add initial dpo script

---
+++ crystalformer/reinforce/dpo.py
@@ -0,0 +1,109 @@
+import jax
+import jax.numpy as jnp
+import optax
+import os
+import math
+
+from crystalformer.src.utils import shuffle
+import crystalformer.src.checkpoint as checkpoint
+
+
+def make_dpo_loss(logp_fn, beta):
+    
+    def dpo_logp_fn(policy_chosen_logps,
+                    policy_rejected_logps,
+                    ref_chosen_logps,
+                    ref_rejected_logps):
+        
+        pi_logratios = policy_chosen_logps - policy_rejected_logps
+        ref_logratios = ref_chosen_logps - ref_rejected_logps
+
+        logits = pi_logratios - ref_logratios
+
+        losses = -jax.nn.log_sigmoid(beta * logits)
+        # chosen_rewards = beta * (policy_chosen_logps - ref_chosen_logps)
+        # rejected_rewards = beta * (policy_rejected_logps - ref_rejected_logps)
+        return jnp.mean(losses)
+    
+    def loss_fn(params, key, x_w, x_l, ref_chosen_logps, ref_rejected_logps):
+        logp_w, logp_xyz, logp_a, logp_l = logp_fn(params, key, *x_w, False)
+        policy_chosen_logps = logp_w + logp_xyz + logp_a + logp_l
+
+        logp_w, logp_xyz, logp_a, logp_l = logp_fn(params, key, *x_l, False)
+        policy_rejected_logps = logp_w + logp_xyz + logp_a + logp_l
+
+        return dpo_logp_fn(policy_chosen_logps,
+                           policy_rejected_logps,
+                           ref_chosen_logps,
+                           ref_rejected_logps)
+
+    return loss_fn
+
+
+def train(key, optimizer, opt_state, dpo_loss_fn, logp_fn, params, epoch_finished, epochs, batchsize, chosen_data, rejected_data, path):
+
+    @jax.jit
+    def step(params, key, opt_state, x_w, x_l, ref_chosen_logps, ref_rejected_logps):
+        value, grad = jax.value_and_grad(dpo_loss_fn)(params, key, x_w, x_l, ref_chosen_logps, ref_rejected_logps)
+        grad = jax.tree_util.tree_map(lambda g_: g_ * -1.0, grad)  # invert gradient for maximization
+        updates, opt_state = optimizer.update(grad, opt_state, params)
+        params = optax.apply_updates(params, updates)
+        return params, opt_state, value
+
+    log_filename = os.path.join(path, "data.txt")
+    f = open(log_filename, "w" if epoch_finished == 0 else "a", buffering=1, newline="\n")
+    if os.path.getsize(log_filename) == 0:
+        f.write("epoch f_mean f_err v_loss v_loss_w v_loss_a v_loss_xyz v_loss_l\n")
+    ref_params = params
+    logp_fn = jax.jit(logp_fn, static_argnums=7)
+
+    key, subkey1, subkey2 = jax.random.split(key, 3)
+    logp_w, logp_xyz, logp_a, logp_l = logp_fn(ref_params, subkey1, *chosen_data, False)
+    ref_chosen_logps = logp_w + logp_xyz + logp_a + logp_l
+    
+    logp_w, logp_xyz, logp_a, logp_l = logp_fn(ref_params, subkey2, *rejected_data, False)
+    ref_rejected_logps = logp_w + logp_xyz + logp_a + logp_l
+    
+    for epoch in range(epoch_finished+1, epochs):
+        key, subkey = jax.random.split(key)
+        chosen_data = shuffle(subkey, chosen_data)
+        rejected_data = shuffle(subkey, rejected_data)  
+
+        idx = jax.random.permutation(subkey, jnp.arange(len(ref_chosen_logps)))
+        ref_chosen_logps = ref_chosen_logps[idx]
+        ref_rejected_logps = ref_rejected_logps[idx]
+
+        _, chosen_L, _, _, _ = chosen_data
+        train_loss = 0.0 
+        num_samples = chosen_L.shape[0]
+        if num_samples % batchsize == 0:
+            num_batches = math.ceil(num_samples / batchsize)
+        else:
+            num_batches = math.ceil(num_samples / batchsize) - 1
+        
+        for batch_idx in range(num_batches):
+            start_idx = batch_idx * batchsize
+            end_idx = min(start_idx + batchsize, num_samples)
+            x_w = jax.tree_map(lambda x: x[start_idx:end_idx], chosen_data)
+            x_l = jax.tree_map(lambda x: x[start_idx:end_idx], rejected_data)
+            ref_chosen_logps_batch = jax.tree_map(lambda x: x[start_idx:end_idx], ref_chosen_logps)
+            ref_rejected_logps_batch = jax.tree_map(lambda x: x[start_idx:end_idx], ref_rejected_logps)
+
+            key, subkey = jax.random.split(key)
+            params, opt_state, loss = step(params, key, opt_state, x_w, x_l, ref_chosen_logps_batch, ref_rejected_logps_batch)
+            train_loss += loss
+        
+        train_loss = train_loss / num_batches
+        f.write( ("%6d" + "  %.6f" + "\n") % (epoch, train_loss))
+
+        if epoch % 5 == 0:
+            ckpt = {"params": params,
+                    "opt_state" : opt_state
+                   }
+            ckpt_filename = os.path.join(path, "epoch_%06d.pkl" %(epoch))
+            checkpoint.save_data(ckpt, ckpt_filename)
+            print("Save checkpoint file: %s" % ckpt_filename)
+
+    f.close()
+
+    return params, opt_state
