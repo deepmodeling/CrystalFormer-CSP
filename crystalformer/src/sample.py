2f041206eb4a9e51e4365a74b95fcacbabdfff0a
Lei Wang <wangleiphy@gmail.com>
2025-09-19 17:38:34 +0800

    do not mask out pad atoms in sampling

--- crystalformer/src/sample.py
+++ crystalformer/src/sample.py
@@ -63,6 +63,7 @@ def sample_x(key, h_x, Kx, top_p, temperature, batchsize):
 def sample_crystal(key, transformer, params, n_max, batchsize, atom_types, wyck_types, Kx, Kl, composition, w_mask, top_p, temperature):
 
     atom_mask = jnp.where(composition>0, jnp.ones((atom_types)), jnp.zeros((atom_types)))
+    atom_mask = atom_mask.at[0].set(1) # mask = 1 for allowed elements
        
     def body_fn(i, state):
         key, G, W, A, X, Y, Z, L = state 
